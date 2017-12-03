16a0ad74d92600b7a441abd2cde65e057a7596f1
bmayle <bmayle17@gmail.comgit config --global user.name bmaylegit add . git config --global user.email you@example.comgit config --global user.email bmayle17@gmail.com>
2017-12-03 10:41:41 -0500

    6 cents equals nickel and penny PASS

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,12 +1,14 @@
 def change_maker(change)
 	change_hash = {}
 
-	if change < 5 && change > 0
-		change_hash ["penny"] = change
-
-	elsif change >= 5 && change <= 9
-		change_hash ["nickel"] = change % 5 
+	if change >= 5 && change <= 9
+		change_hash["nickel"] = change / 5
+		change = change % 5
 	end
-	
+
+	if change < 5 && change > 0
+		change_hash["penny"] = change
+	end
+    
 	change_hash
 end
