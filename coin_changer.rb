36f12f49c53427618192bbc6df1ae28d459204b4
bmayle <bmayle17@gmail.comgit config --global user.name bmaylegit add . git config --global user.email you@example.comgit config --global user.email bmayle17@gmail.com>
2017-12-03 09:57:49 -0500

    5 cents equals nickel FAIL

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,8 +1,13 @@
 def change_maker(change)
 	change_hash = {}
 
-	if change >= 1
+	if change <= 5 && change >= 0
 		change_hash ["penny"] = change
+
+	elsif change >= 5 && change <= 9
+		change_hash ["nickel"] = 1
+		change = change - 5
 	end
+	
 	change_hash
 end
--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -13,4 +13,11 @@ class TestCoinChanger < Minitest::Test
 	def test_assert_that_1_cent_equals_penny
 		assert_equal({"penny" => 1}, change_maker(1))
 	end
+
+	def test_assert_that_4_cent_equals_4_penny
+		assert_equal({"penny" => 4}, change_maker(4))
+	end
+	def test_assert_that_5_cent_equals_1_nickel
+		assert_equal({"nickel" => 1}, change_maker(5))
+	end
 end	
