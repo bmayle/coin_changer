d112e2cfb986f57117bf849ba249468621982ca6
bmayle <bmayle17@gmail.comgit config --global user.name bmaylegit add . git config --global user.email you@example.comgit config --global user.email bmayle17@gmail.com>
2017-12-03 12:06:46 -0500

    Coin Changer Working (changed punctuation)

--- coin_changer.rb
+++ coin_changer.rb
@@ -2,27 +2,27 @@ def change_maker(change)
 	change_hash = {}
 
 	if change >= 100
-		change_hash["dollar"] = change / 100
+		change_hash['dollar'] = change / 100
 		change = change % 100
 	end 
 	
 	if change >= 25 && change <= 99
-		change_hash["quarter"] = change / 25
+		change_hash['quarter'] = change / 25
 		change = change % 25
 	end 
 
 	if change >= 10 && change <= 24
-		change_hash["dime"] = change / 10
+		change_hash['dime'] = change / 10
 		change = change % 10
 	end 
 
 	if change >= 5 && change <= 9
-		change_hash["nickel"] = change / 5
+		change_hash['nickel'] = change / 5
 		change = change % 5
 	end
 
-	if change < 5 && change > 0
-		change_hash["penny"] = change
+	if change > 0 && change < 5
+		change_hash['penny'] = change
 	end
     
 	change_hash
--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -11,19 +11,19 @@ class TestCoinChanger < Minitest::Test
 	end
 
 	def test_assert_that_1_cents_equals_penny
-		assert_equal({"penny" => 1}, change_maker(1))
+		assert_equal({'penny' => 1}, change_maker(1))
 	end
 
 	def test_assert_that_4_cents_equals_4_penny
-		assert_equal({"penny" => 4}, change_maker(4))
+		assert_equal({'penny' => 4}, change_maker(4))
 	end
 
 	def test_assert_that_5_cents_equals_1_nickel
-		assert_equal({"nickel" => 1}, change_maker(5))
+		assert_equal({'nickel' => 1}, change_maker(5))
 	end
 
 	def test_assert_that_6_cents_equals_1_nickel_and_1_penny
-	assert_equal({"nickel" => 1, "penny" => 1}, change_maker(6))
+	assert_equal({'nickel' => 1, 'penny' => 1}, change_maker(6))
 	end
 
 	def test_assert_that_10_cents_equals_1_dime
@@ -31,19 +31,19 @@ class TestCoinChanger < Minitest::Test
 	end
 
 	def test_assert_that_25_cents_equals_1_quarter
-	assert_equal({"quarter" => 1}, change_maker(25))
+	assert_equal({'quarter' => 1}, change_maker(25))
 	end
 
 	def test_assert_that_43_cents_equals_1_quarter_1_dime_1_nickel_and_3_pennies
-	assert_equal({"quarter" => 1, "dime" => 1, "nickel" => 1, "penny" => 3}, change_maker(43))
+	assert_equal({'quarter' => 1, 'dime' => 1, 'nickel' => 1, 'penny' => 3}, change_maker(43))
 	end
 
 	def test_assert_that_100_cents_equals_1_dollar
-	assert_equal({"dollar" => 1}, change_maker(100))
+	assert_equal({'dollar' => 1}, change_maker(100))
 	end
 
 	def test_assert_that_194_cents_equals_1_dollar_3_quarters_1_dime_1_nickel_and_4_pennies
-	assert_equal({"dollar" => 1, "quarter" => 3, "dime" => 1, "nickel" => 1, "penny" => 4}, change_maker(194))
+	assert_equal({'dollar' => 1, 'quarter' => 3, 'dime' => 1, 'nickel' => 1, 'penny' => 4}, change_maker(194))
 	end
 
 end	
