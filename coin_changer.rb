204f14e17078e23800a92b3ea2b3501fb4b98a7a
bmayle <bmayle17@gmail.comgit config --global user.name bmaylegit add . git config --global user.email you@example.comgit config --global user.email bmayle17@gmail.com>
2017-12-03 14:34:26 -0500

    Reduced Size of Coin Changer App

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,29 +1,36 @@
 def change_maker(change)
 	change_hash = {}
-
-	if change >= 100
-		change_hash['dollar'] = change / 100
-		change = change % 100
-	end 
-	
-	if change >= 25 && change <= 99
-		change_hash['quarter'] = change / 25
-		change = change % 25
-	end 
-
-	if change >= 10 && change <= 24
-		change_hash['dime'] = change / 10
-		change = change % 10
-	end 
-
-	if change >= 5 && change <= 9
-		change_hash['nickel'] = change / 5
-		change = change % 5
+	change_value = {'dollar' => 100, 'quarter' => 25, 'dime' => 10, 'nickel' => 5, 'penny' => 1}
+	change_value.each do |money, amount|
+		if change >= amount
+			change_hash[money] = change / amount
+			change = change % amount
+		end	
 	end
-
-	if change > 0 && change < 5
-		change_hash['penny'] = change
-	end
-    
 	change_hash
 end
+
+
+
+
+# 	if change >= 100 
+# 		change_hash['dollar'] = change / 100
+# 		change = change % 100
+# 	end 	
+# 	if change >= 25 && change <= 99
+# 		change_hash['quarter'] = change / 25
+# 		 change = change % 25
+# 	end 
+# 	if change >= 10 && change <= 24
+# 		change_hash['dime'] = change / 10
+# 		 change = change % 10
+# 	end 
+# 	if change >= 5 && change <= 9
+# 		change_hash['nickel'] = change / 5
+# 		 change = change % 5
+# 	end
+# 	if change > 0 && change < 5 
+# 		change_hash['penny'] = change
+# 	end   
+# 	change_hash
+# end
